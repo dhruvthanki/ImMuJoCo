148531f3ce5ed5f53614cf871403bb0ea9e8cfca
Dhruv Thanki <thankid@udel.edu>
2023-10-24 19:59:30 -0500

    drag and drop graph example

--- main.cpp
+++ main.cpp
@@ -136,6 +136,23 @@ struct ScrollingBuffer {
 };
 
 
+template <typename T>
+inline T RandomRange(T min, T max) {
+    T scale = rand() / (T) RAND_MAX;
+    return min + scale * ( max - min );
+}
+
+
+ImVec4 RandomColor() {
+    ImVec4 col;
+    col.x = RandomRange(0.0f,1.0f);
+    col.y = RandomRange(0.0f,1.0f);
+    col.z = RandomRange(0.0f,1.0f);
+    col.w = 1.0f;
+    return col;
+}
+
+
 // main function
 int main(int argc, const char** argv) {
   // load and compile model
@@ -190,29 +207,11 @@ int main(int argc, const char** argv) {
 
   // Setup Dear ImGui style
   ImGui::StyleColorsDark();
-  //ImGui::StyleColorsLight();
 
   // Setup Platform/Renderer backends
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplGlfw_SetCallbacksChainForAllWindows(true);
   ImGui_ImplOpenGL3_Init(glsl_version);
-
-  // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-  // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-  // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-  // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-  // - Read 'docs/FONTS.md' for more instructions and details.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-  // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-  //io.Fonts->AddFontDefault();
-  //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-  //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-  //IM_ASSERT(font != nullptr);
 
   // run main loop, target real-time simulation and 60 fps rendering
   while (!glfwWindowShouldClose(window)) {
@@ -241,6 +240,170 @@ int main(int argc, const char** argv) {
     mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
     mjr_render(viewport, &scn, &con);
 
+    if(ImGui::Begin("My Window")){
+
+    // convenience struct to manage DND items; do this however you like
+    struct MyDndItem {
+        int              Idx;
+        int              Plt;
+        ImAxis           Yax;
+        char             Label[16];
+        ImVector<ImVec2> Data;
+        ImVec4           Color;
+        MyDndItem()        {
+            static int i = 0;
+            Idx = i++;
+            Plt = 0;
+            Yax = ImAxis_Y1;
+            snprintf(Label, sizeof(Label), "%02d Hz", Idx+1);
+            Color = RandomColor();
+            Data.reserve(1001);
+            for (int k = 0; k < 1001; ++k) {
+                float t = k * 1.0f / 999;
+                Data.push_back(ImVec2(t, 0.5f + 0.5f * sinf(2*3.14f*t*(Idx+1))));
+            }
+        }
+        void Reset() { Plt = 0; Yax = ImAxis_Y1; }
+    };
+
+    const int         k_dnd = 20;
+    static MyDndItem  dnd[k_dnd];
+    static MyDndItem* dndx = NULL; // for plot 2
+    static MyDndItem* dndy = NULL; // for plot 2
+
+    // child window to serve as initial source for our DND items
+    ImGui::BeginChild("DND_LEFT",ImVec2(100,400));
+    if (ImGui::Button("Reset Data")) {
+        for (int k = 0; k < k_dnd; ++k)
+            dnd[k].Reset();
+        dndx = dndy = NULL;
+    }
+    for (int k = 0; k < k_dnd; ++k) {
+        if (dnd[k].Plt > 0)
+            continue;
+        ImPlot::ItemIcon(dnd[k].Color); ImGui::SameLine();
+        ImGui::Selectable(dnd[k].Label, false, 0, ImVec2(100, 0));
+        if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
+            ImGui::SetDragDropPayload("MY_DND", &k, sizeof(int));
+            ImPlot::ItemIcon(dnd[k].Color); ImGui::SameLine();
+            ImGui::TextUnformatted(dnd[k].Label);
+            ImGui::EndDragDropSource();
+        }
+    }
+    ImGui::EndChild();
+    if (ImGui::BeginDragDropTarget()) {
+        if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+            int i = *(int*)payload->Data; dnd[i].Reset();
+        }
+        ImGui::EndDragDropTarget();
+    }
+
+    ImGui::SameLine();
+    ImGui::BeginChild("DND_RIGHT",ImVec2(-1,400));
+    // plot 1 (time series)
+    ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels | ImPlotAxisFlags_NoGridLines | ImPlotAxisFlags_NoHighlight;
+    if (ImPlot::BeginPlot("##DND1", ImVec2(-1,195))) {
+        ImPlot::SetupAxis(ImAxis_X1, NULL, flags|ImPlotAxisFlags_Lock);
+        ImPlot::SetupAxis(ImAxis_Y1, "[drop here]", flags);
+        ImPlot::SetupAxis(ImAxis_Y2, "[drop here]", flags|ImPlotAxisFlags_Opposite);
+        ImPlot::SetupAxis(ImAxis_Y3, "[drop here]", flags|ImPlotAxisFlags_Opposite);
+
+        for (int k = 0; k < k_dnd; ++k) {
+            if (dnd[k].Plt == 1 && dnd[k].Data.size() > 0) {
+                ImPlot::SetAxis(dnd[k].Yax);
+                ImPlot::SetNextLineStyle(dnd[k].Color);
+                ImPlot::PlotLine(dnd[k].Label, &dnd[k].Data[0].x, &dnd[k].Data[0].y, dnd[k].Data.size(), 0, 0, 2 * sizeof(float));
+                // allow legend item labels to be DND sources
+                if (ImPlot::BeginDragDropSourceItem(dnd[k].Label)) {
+                    ImGui::SetDragDropPayload("MY_DND", &k, sizeof(int));
+                    ImPlot::ItemIcon(dnd[k].Color); ImGui::SameLine();
+                    ImGui::TextUnformatted(dnd[k].Label);
+                    ImPlot::EndDragDropSource();
+                }
+            }
+        }
+        // allow the main plot area to be a DND target
+        if (ImPlot::BeginDragDropTargetPlot()) {
+            if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+                int i = *(int*)payload->Data; dnd[i].Plt = 1; dnd[i].Yax = ImAxis_Y1;
+            }
+            ImPlot::EndDragDropTarget();
+        }
+        // allow each y-axis to be a DND target
+        for (int y = ImAxis_Y1; y <= ImAxis_Y3; ++y) {
+            if (ImPlot::BeginDragDropTargetAxis(y)) {
+                if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+                    int i = *(int*)payload->Data; dnd[i].Plt = 1; dnd[i].Yax = y;
+                }
+                ImPlot::EndDragDropTarget();
+            }
+        }
+        // allow the legend to be a DND target
+        if (ImPlot::BeginDragDropTargetLegend()) {
+            if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+                int i = *(int*)payload->Data; dnd[i].Plt = 1; dnd[i].Yax = ImAxis_Y1;
+            }
+            ImPlot::EndDragDropTarget();
+        }
+        ImPlot::EndPlot();
+    }
+    // plot 2 (Lissajous)
+    if (ImPlot::BeginPlot("##DND2", ImVec2(-1,195))) {
+        ImPlot::PushStyleColor(ImPlotCol_AxisBg, dndx != NULL ? dndx->Color : ImPlot::GetStyle().Colors[ImPlotCol_AxisBg]);
+        ImPlot::SetupAxis(ImAxis_X1, dndx == NULL ? "[drop here]" : dndx->Label, flags);
+        ImPlot::PushStyleColor(ImPlotCol_AxisBg, dndy != NULL ? dndy->Color : ImPlot::GetStyle().Colors[ImPlotCol_AxisBg]);
+        ImPlot::SetupAxis(ImAxis_Y1, dndy == NULL ? "[drop here]" : dndy->Label, flags);
+        ImPlot::PopStyleColor(2);
+        if (dndx != NULL && dndy != NULL) {
+            ImVec4 mixed((dndx->Color.x + dndy->Color.x)/2,(dndx->Color.y + dndy->Color.y)/2,(dndx->Color.z + dndy->Color.z)/2,(dndx->Color.w + dndy->Color.w)/2);
+            ImPlot::SetNextLineStyle(mixed);
+            ImPlot::PlotLine("##dndxy", &dndx->Data[0].y, &dndy->Data[0].y, dndx->Data.size(), 0, 0, 2 * sizeof(float));
+        }
+        // allow the x-axis to be a DND target
+        if (ImPlot::BeginDragDropTargetAxis(ImAxis_X1)) {
+            if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+                int i = *(int*)payload->Data; dndx = &dnd[i];
+            }
+            ImPlot::EndDragDropTarget();
+        }
+        // allow the x-axis to be a DND source
+        if (dndx != NULL && ImPlot::BeginDragDropSourceAxis(ImAxis_X1)) {
+            ImGui::SetDragDropPayload("MY_DND", &dndx->Idx, sizeof(int));
+            ImPlot::ItemIcon(dndx->Color); ImGui::SameLine();
+            ImGui::TextUnformatted(dndx->Label);
+            ImPlot::EndDragDropSource();
+        }
+        // allow the y-axis to be a DND target
+        if (ImPlot::BeginDragDropTargetAxis(ImAxis_Y1)) {
+            if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+                int i = *(int*)payload->Data; dndy = &dnd[i];
+            }
+            ImPlot::EndDragDropTarget();
+        }
+        // allow the y-axis to be a DND source
+        if (dndy != NULL && ImPlot::BeginDragDropSourceAxis(ImAxis_Y1)) {
+            ImGui::SetDragDropPayload("MY_DND", &dndy->Idx, sizeof(int));
+            ImPlot::ItemIcon(dndy->Color); ImGui::SameLine();
+            ImGui::TextUnformatted(dndy->Label);
+            ImPlot::EndDragDropSource();
+        }
+        // allow the plot area to be a DND target
+        if (ImPlot::BeginDragDropTargetPlot()) {
+            if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("MY_DND")) {
+                int i = *(int*)payload->Data; dndx = dndy = &dnd[i];
+            }
+        }
+        // allow the plot area to be a DND source
+        if (ImPlot::BeginDragDropSourcePlot()) {
+            ImGui::TextUnformatted("Yes, you can\ndrag this!");
+            ImPlot::EndDragDropSource();
+        }
+        ImPlot::EndPlot();
+    }
+    ImGui::EndChild();
+    ImGui::End();
+    }
+
     if(ImGui::Begin("My Window"))
     {
       static ScrollingBuffer sdata2;
@@ -252,14 +415,15 @@ int main(int argc, const char** argv) {
       static float history = 10.0f;
       ImGui::SliderFloat("History",&history,1,30,"%.1f s");
 
-      static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
+      static ImPlotAxisFlags flags = ImPlotAxisFlags_AutoFit;
+      //  = ImPlotAxisFlags_NoTickLabels;
 
       if (ImPlot::BeginPlot("##Scrolling", ImVec2(-1,150))) {
-        ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+        ImPlot::SetupAxes("time (sec)", "position (rad)", flags, flags);
         ImPlot::SetupAxisLimits(ImAxis_X1,t - history, t, ImGuiCond_Always);
         ImPlot::SetupAxisLimits(ImAxis_Y1,-1,1);
-        ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL,0.5f);
-        ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2*sizeof(float));
+        // ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL,0.5f);
+        ImPlot::PlotLine("qpos 6", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2*sizeof(float));
         ImPlot::EndPlot();
       }
       ImGui::End();
@@ -274,9 +438,6 @@ int main(int argc, const char** argv) {
     // glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		// Update and Render additional Platform Windows
-    // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-    //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
     if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
     {
         GLFWwindow* backup_current_context = glfwGetCurrentContext();
